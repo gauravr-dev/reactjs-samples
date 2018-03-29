5dc0e93a81bf07638a8332a9da1c5fa16221bfdb
Gaurav Rajput <gaurav.rajput@ebizontek.com>
2018-03-29 21:16:47 +0530

    Dropdown added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
 import 'semantic-ui-css/semantic.min.css';
 import React, { Component } from 'react';
-import { Segment, Button, List } from 'semantic-ui-react';
+import { Segment, List, Dropdown } from 'semantic-ui-react';
 
 import './App.css';
-
 import HTMLEditor from './Editor';
+
+
+class PageOptionMenu extends Component {
+  constructor(props) {
+    super(props)
+
+    const pageOptions = [
+      {
+        id: "1",
+        name: "Edit",
+        route: "edit",
+        icon: "edit"
+      },
+      {
+        id: "2",
+        name: "View Page",
+        route: "view-page",
+        icon: "eye"
+      },
+      {
+        id: "3",
+        name: "Stats",
+        route: "stats",
+        icon: "graph"
+      },
+      {
+        id: "4",
+        name: "Copy",
+        route: "copy",
+        icon: "copy"
+      },
+      {
+        id: "5",
+        name: "Trash",
+        route: "trash",
+        icon: "trash"
+      }
+    ]
+
+    this.state = { options: pageOptions }
+  }
+
+  render() {
+    return (
+      <Dropdown icon={this.props.icon} pointing="top right" className='link item'>
+        <Dropdown.Menu>
+          {
+            this.state.options.map(function (option, idx, pages) {
+              return (
+                option.name === "trash" ? <React.Fragment><Dropdown.Divider /><Dropdown.Item key={option.name} >{option.name}</Dropdown.Item></React.Fragment> :
+                  <Dropdown.Item key={option.name}>{option.name}</Dropdown.Item>
+              )
+            })
+          }
+        </Dropdown.Menu>
+      </Dropdown>
+    )
+  }
+}
+
 
 class PageList extends Component {
   render() {
     return (
       <React.Fragment>
-        <List width={12}>
+        <List>
           {
             this.props.pages.map(function (page, idx, pages) {
               return (
                 <List.Item key={page.pageId}>
                   <Segment>
                     <List.Content floated='right'>
-                      <Button icon="trello" />
+                      {/* <Button icon=""> */}
+                      <PageOptionMenu icon="trello" />
                     </List.Content>
                     <List.Content>
                       <List.Header as="h2">{page.title}</List.Header>
